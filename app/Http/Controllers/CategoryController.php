e133d861a2d794d43c70c0fdfe14ced5b6cfb560
Husin <Muhamad.husin0412@gmail.com>
2019-11-30 20:08:46 +0700

    e-com

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    public function addCategory(Request $request) {
+        
+        if($request->isMethod('post')) { 
+        $data = $request->all();
+        // echo "<pre>"; print_r($data); die;
+        $category = new Category;
+        $category->name = $data['category_name'];
+        $category->description = $data['description'];
+        $category->url = $data['url'];
+        $category->save();
+        return redirect ('/admin/view-categories')->with('flash_message_success','Category Added Successfully!');
+
+      }
+        return view('admin.categories.add_category');
+    }
+
+    public function editCategory(Request $request, $id = null) {
+
+        if($request->isMethod('post')) {
+            $data = $request->all();
+            // echo "<pre>"; print_r($data); die;
+            Category::where(['id'=>$id])->update(['name'=>$data['category_name'],'description'=>$data['description'],'url'=>$data['url']]);
+            return redirect('/admin/view-categories')->with('flash_message_success','Category Updated Successfully!');
+        }
+        $categoryDetails = Category::where(['id'=>$id])->first();
+        return view('admin.categories.edit_category')->with(compact('categoryDetails'));
+    }
+
+    public function deleteCategory($id = null) {
+
+        if(!empty($id)) {
+            Category::where(['id'=>$id])->delete();
+            return redirect()->back()->with('flash_message_success','Category Deleted Successfully!');
+        }
+    }
+
+    public function viewCategories() {
+
+        $categories = Category::get();
+        $categories = json_decode(json_encode($categories));
+        // dd($categories);
+        return view ('admin.categories.view_categories')->with(compact('categories'));
+    }
+
+}
---
+++ public/js/backend_js/matrix.form_validation.js
@@ -0,0 +1,177 @@
+
+// settings validation
+$(document).ready(function(){
+
+	$("#current_pwd").keyup(function() {
+		 var current_pwd = $("#current_pwd").val();
+		 $.ajax({
+			 type:'get',
+			 url:'/admin/check-pwd',
+			 data:{current_pwd:current_pwd},
+			 success:function(resp) {
+				//  alert(resp);
+				if(resp == "false") {
+					$("#chkPwd").html("<font color='red'>Current Password is Incorrect </font>");
+				}else if (resp=="true"){
+                    $("#chkPwd").html("<font color='green'>Current Password is Correct </font>");
+				}
+			 },error:function() {
+				 alert("Error");
+			 }
+		 });
+	});
+	
+	$('input[type=checkbox],input[type=radio],input[type=file]').uniform();
+	
+	$('select').select2();
+	
+	// Form Validation
+    $("#basic_validate").validate({
+		rules:{
+			required:{
+				required:true
+			},
+			email:{
+				required:true,
+				email: true
+			},
+			date:{
+				required:true,
+				date: true
+			},
+			url:{
+				required:true,
+				url: true
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+
+	// Add category Validation
+    $("#add_category").validate({
+		rules:{
+			required:{
+				required:true
+			},
+			category_name:{
+				required:true,
+			},
+			description:{
+				required:true,
+			},
+			url:{
+				required:true,
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+
+		// Add category Validation
+	$("#edit_category").validate({
+		rules:{
+			required:{
+				required:true
+			},
+			category_name:{
+				required:true,
+			},
+			description:{
+				required:true,
+			},
+			url:{
+				required:true,
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+	
+	$("#number_validate").validate({
+		rules:{
+			min:{
+				required: true,
+				min:10
+			},
+			max:{
+				required:true,
+				max:24
+			},
+			number:{
+				required:true,
+				number:true
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+	
+	$("#password_validate").validate({
+		rules:{
+			current_pwd:{
+				required: true,
+				minlength:6,
+				maxlength:20
+			},
+			new_pwd:{
+				required:true,
+				minlength:6,
+				maxlength:20,
+				equalTo:"#new_pwd"
+			},
+			confirm_pwd:{
+				required:true,
+				minlength:6,
+				maxlength:20,
+				equalTo:"#confirm_pwd"
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+
+	$("#delCat").click(function(){
+
+		if(confirm('Are you sure you want to delete this category?')) {
+			return true;
+		}
+		return false;
+	});
+
+});
