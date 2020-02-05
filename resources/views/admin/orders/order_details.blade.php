d9c10f0b68e48f6ceb999664106498adc7421226
Husin <Muhamad.husin0412@gmail.com>
2020-02-05 17:03:24 +0700

    ok

--- resources/views/admin/orders/order_details.blade.php
+++ resources/views/admin/orders/order_details.blade.php
@@ -8,6 +8,7 @@ order details
 
 
 <!--main-container-part-->
+<div id="loading"></div>
 <div id="content">
     <div id="content-header">
         <div id="breadcrumb"> <a href="#" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a
@@ -32,6 +33,26 @@ order details
                                     <td class="taskDesc"> Order Status</td>
                                     <td class="taskStatus">{{$orderDetails->order_status}}</td>
                                 </tr>
+                                <tr>
+                                    <td class="taskDesc"> Order Total</td>
+                                    <td class="taskStatus">Rp {{is_number($orderDetails->grant_total)}}</td>
+                                </tr>
+                                <tr>
+                                    <td class="taskDesc"> Shipping Charges</td>
+                                    <td class="taskStatus">{{$orderDetails->shipping_changes}}</td>
+                                </tr>
+                                <tr>
+                                    <td class="taskDesc"> Coupon Code</td>
+                                    <td class="taskStatus">{{$orderDetails->coupon_code}}</td>
+                                </tr>
+                                <tr>
+                                    <td class="taskDesc"> Coupon Amount</td>
+                                    <td class="taskStatus">Rp {{is_number($orderDetails->coupon_amount)}}</td>
+                                </tr>
+                                <tr>
+                                    <td class="taskDesc"> Payment Method</td>
+                                    <td class="taskStatus">{{$orderDetails->payment_method}}</td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
@@ -44,12 +65,21 @@ order details
                             </div>
                         </div>
                         <div class="accordion-body in collapse" id="collapseGOne" style="">
-                            <div class="widget-content"> Billing details will come here.. </div>
+                            <div class="widget-content">
+                                <li>{{$userDetails->name}}</li> <br>
+                                <li>{{$userDetails->address}}</li> <br>
+                                <li>{{$userDetails->city}}</li> <br>
+                                <li>{{$userDetails->state}}</li> <br>
+                                <li>{{$userDetails->country}}</li> <br>
+                                <li>{{$userDetails->pincode}}</li> <br>
+                                <li>{{$userDetails->mobile}}</li> <br>
+                                <li>{{$userDetails->email}}</li> <br>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            
+
             <div class="span6">
                 <div class="widget-box">
                     <div class="widget-title">
@@ -74,11 +104,34 @@ order details
                     <div class="accordion-group widget-box">
                         <div class="accordion-heading">
                             <div class="widget-title">
+                                <h5>Update Order Status</h5>
+                            </div>
+                        </div>
+                        <div class="accordion-body in collapse" id="collapseGOne" style="">
+                            <div class="widget-content">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="accordion" id="collapse-group">
+                    <div class="accordion-group widget-box">
+                        <div class="accordion-heading">
+                            <div class="widget-title">
                                 <h5>Shipping Address</h5>
                             </div>
                         </div>
                         <div class="accordion-body in collapse" id="collapseGOne" style="">
-                            <div class="widget-content"> Shipping address will come here.. </div>
+                            <div class="widget-content">
+                                <li>{{$orderDetails->name}}</li> <br>
+                                <li>{{$orderDetails->address}}</li> <br>
+                                <li>{{$orderDetails->city}}</li> <br>
+                                <li>{{$orderDetails->state}}</li> <br>
+                                <li>{{$orderDetails->country}}</li> <br>
+                                <li>{{$orderDetails->pincode}}</li> <br>
+                                <li>{{$orderDetails->mobile}}</li> <br>
+                                <li>{{$orderDetails->user_email}}</li> <br>
+                            </div>
                         </div>
                     </div>
 
@@ -86,6 +139,42 @@ order details
             </div>
         </div>
         <hr>
+        <div class="container-fluid">
+            <table id="example" class="table table-striped table-bordered" style="width:100%;">
+                <thead>
+                    <tr>
+                        <th style="text-align:center; width: 12%;">Product Code</th>
+                        <th style="text-align:center; width: 16%;">Product Name</th>
+                        <th style="text-align:center; width: 15%;">Product Size</th>
+                        <th style="text-align:center;">Product Color</th>
+                        <th style="text-align:center;">Product Price</th>
+                        <th style="text-align:center;">Product Qty</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($orderDetails->orders as $pro)
+                    <tr>
+                        <td style="text-align:center;">{{$pro->product_code}}</td>
+                        <td style="text-align:center;">{{$pro->product_name}}</td>
+                        <td style="text-align:center;">{{$pro->product_size}}</td>
+                        <td style="text-align:center;">{{$pro->product_color}}</td>
+                        <td style="text-align:center;">Rp {{is_number($pro->product_price)}}</td>
+                        <td style="text-align:center;">{{$pro->product_qty}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th style="text-align:center; width: 9%;">Product Code</th>
+                        <th style="text-align:center; width: 14%;">Product Name</th>
+                        <th style="text-align:center; width: 15%;">Product Size</th>
+                        <th style="text-align:center;">Product Color</th>
+                        <th style="text-align:center;">Product Price</th>
+                        <th style="text-align:center;">Product Qty</th>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
     </div>
 </div>
 <!--main-container-part-->
